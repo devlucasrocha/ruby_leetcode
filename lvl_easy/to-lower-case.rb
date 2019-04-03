85b326e5444747df4f2fb23fc4dde558a43d2bbf
devlucasrocha <lucasgeorgerocha@gmail.com>
2019-04-03 09:49:41 -0300

    To Lower Case

---
+++ lvl_easy/to-lower-case.rb
@@ -0,0 +1,7 @@
+# Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+def to_lower_case(str)
+    str.downcase
+end
+
+print to_lower_case('HO')
